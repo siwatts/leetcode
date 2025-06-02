f34c79b0e8bfb7e83451de600a485c810912163e
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-06-02 18:27:43 +0100

    02/06/25 two-sum & candy C#

---
+++ candy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+
+public class Solution
+{
+    public static int Candy(int[] ratings)
+    {
+        // n children in a line
+        // each with rating at index
+        // must have at least 1 candy each
+        // higher rating = more candies than their immediate neighbours
+        // find min. no. of candies req'd
+
+        // Everyone must have at least 1 candy
+        int[] candies = Enumerable.Repeat(1, ratings.Length).ToArray<int>();
+
+        // Step through
+        for (int i = 1; i < ratings.Length; i++)
+        {
+            // If next child has higher or lower than prev. then extra candy needed for one of them
+            if (ratings[i] != ratings[i-1])
+            {
+                if (ratings[i] > ratings[i-1])
+                {
+                    // One more than previous kids
+                    candies[i] = candies[i-1] + 1;
+                }
+                else
+                {
+                    // Previous kid needs 1 more than this kid *at least*
+                    // He may have already had higher because we already dealt with him before
+                    // so don't downgrade him!
+                    candies[i-1] = Math.Max(candies[i] + 1, candies[i-1]);
+                }
+            }
+        }
+
+        return candies.Sum();
+    }
+}
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("Candy");
+        int[] ratings = [1,0,2];
+        int output = Solution.Candy(ratings);
+        Console.WriteLine("Output: {0}", output);
+
+        return 0;
+    }
+}
+
---
+++ two-sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+
+public class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        // Indices of the 2 elements from num that sum to target
+        // Assume exactly 1 sol'n per problem
+
+        Console.WriteLine("Nums length: {0}", nums.Length);
+        Console.WriteLine("Target: {0}", target);
+        Console.WriteLine("Running for loop from 0 to {0}", nums.Length);
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine("For loop i = {0}", i);
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                Console.WriteLine("For loop j = {0}", j);
+                // Dumb direct index access method, no fancy linq
+                if (nums[i] + nums[j] == target)
+                {
+                    Console.WriteLine("Found target {0} at [{1}, {2}]", target, i, j);
+                    int[] res = [i, j];
+                    return res;
+                }
+                else
+                {
+                    Console.WriteLine("Target not found {0} at [{1}, {2}]", target, i, j);
+                }
+            }
+        }
+        return null;
+    }
+}
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        int[] nums = [3,2,4];
+        int target = 6;
+
+        var result = Solution.TwoSum(nums, target);
+        Console.WriteLine("Result [{0}, {1}]", result[0], result[1]);
+
+        return 0;
+    }
+}
