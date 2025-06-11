4d3b1ff63dcd587b42e5295a5b4e7b6bfca3dedb
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-06-11 12:50:53 +0100

    Make Solution class non-static and remove prints

--- 2-add-two-numbers/program.cc
+++ 2-add-two-numbers/program.cc
@@ -33,24 +33,24 @@ class Solution
 {
     public:
         // Setup
-        static void printList(ListNode* list)
+        void printList(ListNode* list)
         {
-            cout << "Printing list contents\n";
-            cout << "[";
+            //cout << "Printing list contents\n";
+            //cout << "[";
             printNode(list);
-            cout << "]\n";
+            //cout << "]\n";
         }
-        static void printNode(ListNode* node)
+        void printNode(ListNode* node)
         {
-            cout << node->val;
+            //cout << node->val;
             // Recursive
             if (node->next != nullptr)
             {
-                cout << ",";
+                //cout << ",";
                 printNode(node->next);
             }
         }
-        static ListNode* makeListFromNumber(int input)
+        ListNode* makeListFromNumber(int input)
         {
             // 1st (biggest) digit of input is the last element of list
             // Since we make list starting from last element and link each next element
@@ -58,7 +58,7 @@ class Solution
             //
             // The digits are pulled off starting from the least significant, so use a
             // stack to hold them and flip the order
-            cout << "Given " << input << " to parse\n";
+            //cout << "Given " << input << " to parse\n";
             stack<int> digits;
             int digit;
             while (input != 0)
@@ -66,27 +66,27 @@ class Solution
                 // Pull off least significant digit
                 digit = input % 10;
                 digits.push(digit);
-                cout << "Found digit " << digit << endl;
+                //cout << "Found digit " << digit << endl;
                 // Shift back by 10
                 input -= digit;
                 input = (int) (input / 10.0);
-                cout << "Now " << input << endl;
+                //cout << "Now " << input << endl;
             }
-            cout << "Done parsing input\n" << endl;
+            //cout << "Done parsing input\n" << endl;
             // Make linked list, starting with last element (most significant bit)
             ListNode* prev = nullptr;
             while (!digits.empty())
             {
                 digit = digits.top();
                 digits.pop();
-                cout << "Making node from digit " << digit << endl;
+                //cout << "Making node from digit " << digit << endl;
                 ListNode* n = new ListNode(digit, prev);
                 prev = n;
             }
             // List complete, and first node is the previously created one
             return prev;
         }
-        static void deleteList(ListNode* list)
+        void deleteList(ListNode* list)
         {
             if (list->next != nullptr)
             {
@@ -96,7 +96,7 @@ class Solution
             delete list;
         }
         // Solution
-        static int numberFromList(ListNode* list)
+        int numberFromList(ListNode* list)
         {
             // Traverse over list which starts with the least signifcant bit
             // Increase the power of 10 for each digit we read by 1
@@ -111,12 +111,12 @@ class Solution
             }
             return result;
         }
-        static ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
         {
             int n1 = numberFromList(l1);
             int n2 = numberFromList(l2);
             int res = n1 + n2;
-            cout << "Decoding list addition: " << n1 << " + " << n2 << " = " << res << "\n";
+            //cout << "Decoding list addition: " << n1 << " + " << n2 << " = " << res << "\n";
             return makeListFromNumber(res);
         }
 };
@@ -132,20 +132,22 @@ int main(int argc, char* argv[])
     // ListNode first(3, &mid);
     // Solution::printList(&first);
 
-    ListNode* l1 = Solution::makeListFromNumber(342);
-    ListNode* l2 = Solution::makeListFromNumber(465);
-    Solution::printList(l1);
-    Solution::printList(l2);
+    Solution soln;
+
+    ListNode* l1 = soln.makeListFromNumber(342);
+    ListNode* l2 = soln.makeListFromNumber(465);
+    soln.printList(l1);
+    soln.printList(l2);
 
     // Add together
-    ListNode* output = Solution::addTwoNumbers(l1, l2);
+    ListNode* output = soln.addTwoNumbers(l1, l2);
     // Display output
-    Solution::printList(output);
+    soln.printList(output);
 
     // Cleanup
-    Solution::deleteList(l1);
-    Solution::deleteList(l2);
-    Solution::deleteList(output);
+    soln.deleteList(l1);
+    soln.deleteList(l2);
+    soln.deleteList(output);
 
     return 0;
 }
