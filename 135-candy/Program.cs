8171da535640de7c298a65429a852cf836c0a597
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-06-12 19:00:31 +0100

    Switch to consecutive chain approach, new bug
    - Now double counts in some situations (see test case)

--- 135-candy/Program.cs
+++ 135-candy/Program.cs
@@ -11,30 +11,45 @@ public class Solution
         // find min. no. of candies req'd
 
         // Everyone must have at least 1 candy
-        int[] candies = Enumerable.Repeat(1, ratings.Length).ToArray<int>();
+        int count = ratings.Length;
+        //int[] candies = Enumerable.Repeat(1, ratings.Length).ToArray<int>();
 
         // Step through
+        // Find longest unbroken chains of increases and decreases, as the candies will stack
+        int increaseCount = 0;
+        int decreaseCount = 0;
         for (int i = 1; i < ratings.Length; i++)
         {
-            // If next child has higher or lower than prev. then extra candy needed for one of them
-            if (ratings[i] != ratings[i-1])
+            // If next child has higher than prev. then extra candy needed for one of them
+            if (ratings[i] > ratings[i - 1])
             {
-                if (ratings[i] > ratings[i-1])
-                {
-                    // One more than previous kids
-                    candies[i] = candies[i-1] + 1;
-                }
-                else
-                {
-                    // Previous kid needs 1 more than this kid *at least*
-                    // He may have already had higher because we already dealt with him before
-                    // so don't downgrade him!
-                    candies[i-1] = Math.Max(candies[i] + 1, candies[i-1]);
-                }
+                increaseCount++;
+                // Account for candies in the increase chain up to now
+                // 1 increase = 1, 3 consecutive increases = 3 + 2 + 1 and so on
+                count += increaseCount;
+            }
+            else if (increaseCount != 0)
+            {
+                // Reset
+                increaseCount = 0;
+            }
+
+            // If next child has lower than prev. then extra candy needed for one of them
+            if (ratings[i] < ratings[i - 1])
+            {
+                decreaseCount++;
+                // Account for candies in the decrease chain up to now
+                // 1 decrease = 1, 3 consecutive decreases = 3 + 2 + 1 and so on
+                count += decreaseCount;
+            }
+            else if (decreaseCount != 0)
+            {
+                // Reset
+                decreaseCount = 0;
             }
         }
 
-        return candies.Sum();
+        return count;
     }
 }
 
@@ -43,10 +58,16 @@ public class Program
     public static int Main(string[] args)
     {
         Console.WriteLine("Candy");
-        int[] ratings = [1,0,2];
+        int[] ratings = [1, 3, 2, 2, 1];
+        int expAnswer = 7; // -1 to ignore
+
         Solution soln = new Solution();
         int output = soln.Candy(ratings);
         Console.WriteLine("Output: {0}", output);
+        if (expAnswer != -1 && output != expAnswer)
+        {
+            Console.WriteLine("Incorrect answer, the expected answer is {0}", expAnswer);
+        }
 
         return 0;
     }
