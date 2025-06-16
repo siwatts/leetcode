00c60e9e431d869415b54ea1b46a1a57293c23fc
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-06-16 17:21:04 +0100

    Handle an edge case where 3 individual char. are given

--- 2423-remove-letter-equalise-freq/program.cc
+++ 2423-remove-letter-equalise-freq/program.cc
@@ -36,6 +36,7 @@ public:
         }
 
         // 2+ elements, so we require 1 count to be 1 higher than all the others which are the same
+        // OR we require 3 elements to be 1 e.g. 'abc' where any 1 can be removed
         unordered_map<int,int> countFrequency;
         for (auto &[c, n] : charFrequency)
         {
@@ -44,7 +45,12 @@ public:
 
         // Now we have map of countFrequency, ie. how many times that character count has occurred
         // We want to see 2 entries, a count with frequency of 2+, and a count 1 higher with frequency of 1
-        if (countFrequency.size() != 2)
+        // OR we require 1 entry, a count of 1 with a freq. of 3 (the special case mentioned above 'abc')
+        if (countFrequency.size() == 1)
+        {
+            return countFrequency[1] == 3;
+        }
+        else if (countFrequency.size() != 2)
         {
             return false;
         }
