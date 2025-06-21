9f0d885c78a6a7d7418abea0bc485e8dcf0bc788
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-06-22 00:13:20 +0100

    New faster solution, more abstract
    - Track character counts and how many char there are with this count,
      we don't actually care about the char themselves

--- 3085-min-del-make-str-k-special/program.cc
+++ 3085-min-del-make-str-k-special/program.cc
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <string>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -25,9 +26,8 @@ class Solution
     int k;
     // Dynamic
     //string word;
-    char smallestChar;
-    long smallestCharCount;
-    unordered_map<char,long> charCount;
+    queue<long> charCountsQueue;
+    unordered_map<long,long> charCountFreq;
 
 public:
     // Return the baseline min deletions required that can be achieved without
@@ -35,62 +35,72 @@ public:
     long minimumDeletionsNoCharRemove()
     {
         //cout << "Called 'long minimumDeletionsNoCharRemove()'\n";
-        long min = originalWord.length();
-        for (auto& [c, n] : charCount)
-        {
-            if (n < min && n != 0)
-            {
-                min = n;
-                smallestChar = c;
-                smallestCharCount = n;
-            }
-        }
+        long min = charCountsQueue.front();
+
         //cout << "this min = " << min << "\n";
         // Whatever the min char. count is, is what we need every char to be
         // Within a tolerance of k
         long remove = 0;
-        for (auto& [c, n] : charCount)
+        for (auto& [n, f] : charCountFreq)
         {
-            //cout << "c = '" << c << "', n = " << n << "\n";
-            long sub = max<long>(n - min - k, 0);
-            //cout << "subtract " << sub << " to bring char within k = " << k << " of min = " << min << "\n";
+            long sub = max<long>(f * (n - min - k), 0);
             remove += sub;
-            //cout << "remove now = " << remove << "\n";
         }
         return remove;
     }
     int minimumDeletions(string word, int k)
     {
+        this->originalWord = word;
+        this->k = k;
         // Build the char count map, so we can ignore the string from now on
+        unordered_map<char,long> charCount;
+        vector<long> charCounts;
+        //cout << "Count char...\n";
         for (auto& c : word)
         {
             charCount[c]++;
         }
-        this->originalWord = word;
-        this->k = k;
+        //cout << "Count char freq...\n";
+        for (auto& [c,n] : charCount)
+        {
+            charCounts.push_back(n);
+            charCountFreq[n]++;
+        }
+        //cout << "Unique charcounts...\n";
+        charCounts.erase( unique( charCounts.begin(), charCounts.end() ), charCounts.end() );
+        //cout << "Sort charcounts...\n";
+        sort( charCounts.begin(), charCounts.end() );
+        //cout << "Push charcounts...\n";
+        for (long i : charCounts)
+        {
+            charCountsQueue.push(i);
+        }
 
         // Get a baseline
         long min = minimumDeletionsNoCharRemove();
 
         long charRemovalCost {0};
         // Should we iterate?
-        while (smallestCharCount < min)
+        long smallestCount;
+        long thisScore;
+        while (charCountsQueue.size() > 0 && charCountsQueue.front() < min)
         {
             // There may be a better solution we can find via brute force
             //cout << "smallestCharCount = " << smallestCharCount << " < min = " << min << "\n";
             //cout << "  Trying removal of smallest char = '" << smallestChar << "'\n";
 
-            // Remove the char which occurs the fewest times and try again
-            charCount.erase(smallestChar);
+            // Remove the chars which occurs the fewest times and try again
+            smallestCount = charCountsQueue.front();
+            charCountsQueue.pop();
+            charRemovalCost += smallestCount * charCountFreq[smallestCount];
+            charCountFreq.erase(smallestCount);
             // Keep a cumulative score that persists outside the loop
             // otherwise we forget the pre-baked in cost of prior char removals
-            charRemovalCost += smallestCharCount; // How much it cost to remove it
-            //cout << "cumulative scoreSoFar = " << charRemovalCost << " to remove all smallest char removed so far\n";
 
             // Iterate next cost to see if it is better
             // This method has side effects to the class properties. Make this more clear?
             // Look at this one individually
-            long thisScore = charRemovalCost + minimumDeletionsNoCharRemove();
+            thisScore = charRemovalCost + minimumDeletionsNoCharRemove();
             //cout << "Combined score after removal of smallest char now = " << thisScore << "\n";
 
             // Only keep the new one if it is better!
