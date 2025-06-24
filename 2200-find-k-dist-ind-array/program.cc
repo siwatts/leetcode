eabe0d1c4bc5dcee09c0efc1d7edfd1bfafd7563
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-06-24 12:44:34 +0100

    2200 working solution (slow)

---
+++ 2200-find-k-dist-ind-array/program.cc
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+/*
+You are given a 0-indexed integer array nums and two integers key and k. A
+k-distant index is an index i of nums for which there exists at least one index
+j such that |i - j| <= k and nums[j] == key.
+
+Return a list of all k-distant indices sorted in increasing order.
+*/
+
+class Solution
+{
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k)
+    {
+        int N = nums.size();
+        vector<int> keyInd;
+        for (int i = 0; i < N; i++)
+        {
+            if (nums[i] == key)
+            {
+                keyInd.push_back(i);
+            }
+        }
+
+        set<int> ind;
+        for (int i : keyInd)
+        {
+            int start = (i - k) >= 0 ? i - k : 0;
+            int end = (i + k) < N ? i + k : N-1;
+            for (int j = start; j <= end; j++)
+            {
+                ind.insert(j);
+            }
+        }
+
+        vector<int> out(ind.begin(), ind.end());
+
+        return out;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    cout << "2200-find-k-dist-ind-array\n";
+
+    vector<int> nums;
+    int key;
+    int k;
+    vector<int> exp;
+    if (argc > 3)
+    {
+        // Array from arg 1 -> N-3
+        // key at arg N-2
+        // k at arg N-1
+        for (int i = 1; i <= argc-3; i++)
+        {
+            nums.push_back(atoi(argv[i]));
+        }
+        key = atoi(argv[argc-2]);
+        k = atoi(argv[argc-3]);
+    }
+    else
+    {
+        nums = { 3,4,9,1,3,9,5 };
+        key = 9;
+        k = 1;
+        exp = { 1,2,3,4,5,6 };
+    }
+
+    Solution sol;
+    vector<int> res = sol.findKDistantIndices(nums, key, k);
+
+    cout << "Input: [";
+    for (auto n : nums)
+    {
+        cout << n << ",";
+    }
+    cout << "], key = " << key << ", k = " << k << "\n";
+    cout << "Output: [";
+    for (auto n : res)
+    {
+        cout << n << ",";
+    }
+    cout << "]\n";
+
+    return 0;
+}
+
+
