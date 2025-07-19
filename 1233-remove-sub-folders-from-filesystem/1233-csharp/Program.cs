d1294154335c26a0668d90558fa453d54082f5bc
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-07-20 00:03:10 +0100

    1233 C# WIP

---
+++ 1233-remove-sub-folders-from-filesystem/1233-csharp/Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+Given a list of folders folder, return the folders after removing all
+sub-folders in those folders. You may return the answer in any order.
+
+If a folder[i] is located within another folder[j], it is called a sub-folder of
+it. A sub-folder of folder[j] must start with folder[j], followed by a "/". For
+example, "/a/b" is a sub-folder of "/a", but "/b" is not a sub-folder of
+"/a/b/c".
+
+The format of a path is one or more concatenated strings of the form: '/'
+followed by one or more lowercase English letters.
+
+- For example, "/leetcode" and "/leetcode/problems" are valid paths while an
+empty string and "/" are not.
+*/
+
+public class Solution
+{
+    public IList<string> RemoveSubfolders(string[] folder)
+    {
+        // Initialise to dictionary with strings -> key, and every value to 1
+        // There will be an exception if there are duplicate keys
+        // If this exists in the test data use .Distinct first.
+        Dictionary<string, int> paths = folder.ToDictionary(x => x, x => 1);
+
+        List<string> res = new List<string>();
+        foreach (var f in folder)
+        {
+            List<string> dirNames = f.Split('/').ToList<string>();
+            bool isSubDir = false;
+            if (dirNames.Count != 1)
+            {
+                // Check if it is a subdir of any others in the dict.
+                // For this we need to gradually combine from the root up and test
+                // each, e.g. '/a/b/c/d` we should test `/a`, `/a/b`, `/a/b/c`
+                string baseDir = "";
+                for (int i = 0; i < dirNames.Count-1; i++)
+                {
+                    baseDir += "/" + dirNames[i];
+                    if (paths.ContainsKey(baseDir))
+                    {
+                        isSubDir = true;
+                        break;
+                    }
+                }
+            }
+            // Now we know the result of whether it was a subdir of something
+            // in the dictionary or not, so add if we're good
+            if (!isSubDir)
+            {
+                res.Add(f);
+            }
+        }
+
+        return res;
+    }
+}
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("1233-remove-sub-folders-from-filesystem\n");
+
+        string[] folder = [ "/a","/a/b","/c/d","/c/d/e","/c/f" ];
+        string[] exp = [ "/a","/c/d","/c/f" ];
+
+        Solution sol = new Solution();
+        List<string> res = new List<string>(sol.RemoveSubfolders(folder));
+
+        Console.Write("Input: folder = [ ");
+        foreach (var f in folder)
+        {
+            Console.Write("'{0}' ", f);
+        }
+        Console.WriteLine("]");
+        Console.Write("Output: res = [ ");
+        foreach (var f in res)
+        {
+            Console.Write("'{0}' ", f);
+        }
+        Console.WriteLine("]");
+
+        return 0;
+    }
+}
