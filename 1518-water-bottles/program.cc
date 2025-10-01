4cfb265d1e0a9f7aacb40809bf9e3580daf82735
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-10-01 17:05:30 +0100

    1518 working example 1

---
+++ 1518-water-bottles/program.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+There are numBottles water bottles that are initially full of water. You can
+exchange numExchange empty water bottles from the market with one full water
+bottle.
+
+The operation of drinking a full water bottle turns it into an empty bottle.
+
+Given the two integers numBottles and numExchange, return the maximum number of
+water bottles you can drink.
+*/
+
+class Solution
+{
+public:
+    int numWaterBottles(int numBottles, int numExchange)
+    {
+        if (numExchange == 0)
+        {
+            return numBottles;
+        }
+
+        // Drink all initially
+        int res = numBottles;
+
+        // If more empties than required for exchange, exchange all we can
+        do
+        {
+            numBottles = numBottles / numExchange;
+            res += numBottles;
+        }
+        while (numBottles > 0);
+
+        return res;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    cout << "1518-water-bottles\n";
+
+    int numBottles, numExchange, exp;
+    if (argc > 3)
+    {
+        numBottles = stoi(argv[1]);
+        numExchange = stoi(argv[2]);
+        exp = stoi(argv[3]);
+    }
+    else
+    {
+        numBottles = 9;
+        numExchange = 3;
+        exp = 13;
+    }
+    cout << "numBottles = " << numBottles << ", numExchange = " << numExchange << ", exp = " << exp << "\n";
+
+    Solution sol;
+    int res = sol.numWaterBottles(numBottles, numExchange);
+
+    cout << "Res = " << res << "\n";
+    if (res == exp)
+    {
+        cout << "Test: PASS\n";
+    }
+    else
+    {
+        cout << "Test: FAIL\n";
+    }
+
+    return 0;
+}
+
