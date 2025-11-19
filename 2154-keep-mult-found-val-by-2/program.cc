fe0a9d6575fb5122723f60fef3734a36aa779c1d
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-11-19 00:22:50 +0000

    2154 working solution

---
+++ 2154-keep-mult-found-val-by-2/program.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+/*
+You are given an array of integers nums. You are also given an integer original
+which is the first number that needs to be searched for in nums.
+
+You then do the following steps:
+
+- If original is found in nums, multiply it by two (i.e., set original = 2 * original).
+- Otherwise, stop the process.
+- Repeat this process with the new number as long as you keep finding the number.
+
+Return the final value of original.
+*/
+
+class Solution
+{
+public:
+    int findFinalValue(vector<int>& nums, int original)
+    {
+        set<int> s;
+        for (auto& n: nums)
+        {
+            s.insert(n);
+        }
+
+        while (s.contains(original))
+        {
+            original *= 2;
+        }
+
+        return original;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    cout << "2154-keep-mult-found-val-by-2\n";
+
+    vector<int> nums = { 5,3,6,1,12 };
+    int original = 3;
+
+    Solution sol;
+    int res = sol.findFinalValue(nums, original);
+
+    cout << "Input: nums = [ ";
+    for (auto& n: nums)
+    {
+        cout << n << ",";
+    }
+    cout << " ], original = " << original << "\n";
+    cout << "Output: res = " << res << "\n";
+
+    return 0;
+}
+
