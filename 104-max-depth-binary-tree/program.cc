1a8af93e55c7da67610681fd87bf28e6dd1f07f7
Simon Watts <14802483+siwatts@users.noreply.github.com>
2026-02-24 17:36:38 +0000

    104 most basic dfs binary tree

---
+++ 104-max-depth-binary-tree/program.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/*
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from
+the root node down to the farthest leaf node.
+*/
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode* root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    cout << "104-max-depth-binary-tree\n";
+
+    return 0;
+}
+
